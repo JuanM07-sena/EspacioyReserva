1d6c68d8a8a1544750b1506d1d7683c00395074c
JuanM07 <juanmsena07@gmail.com>
2025-11-13 11:54:48 -0500

    Inicio

---
+++ database/migrations/2025_11_13_154901_create_espacios_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEspaciosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('espacios', function (Blueprint $table) {
+           $table->id();
+            $table->string('nombre')->require();
+            $table->string('tipo')->require();
+            $table->unsignedInteger('capacidad' )->require();
+            $table->string('ubicacion')->require();
+            $table->timestamps();
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('espacios');
+    }
+}
